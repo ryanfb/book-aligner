bd602de95ef74c962b55d9739adf4e74634cfaae
Ryan Baumann <ryan.baumann@gmail.com>
2016-03-09 13:40:53 -0500

    First pass at publication-year filtering

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ all: alignment-unique.csv
 alignment-unique.csv: alignment.csv
 	sort $^ | uniq > $@
 
-alignment.csv: ia-oclc-lccn-issn-isbn.csv ia-volumes.csv hathifile.tsv book-aligner.rb
-	./book-aligner.rb ia-oclc-lccn-issn-isbn.csv ia-volumes.csv hathifile.tsv > $@
+alignment.csv: ia-oclc-lccn-issn-isbn-vol-pub.csv hathifile.tsv book-aligner.rb
+	./book-aligner.rb ia-oclc-lccn-issn-isbn-vol-pub.csv hathifile.tsv > $@
 
 hathifile.tsv:
 	curl 'https://www.hathitrust.org/filebrowser/download/142568' | gunzip -c > $@
@@ -16,10 +16,7 @@ ia-oclc-lccn-issn-isbn-vol.csv:
 	curl -o $@ 'https://archive.org/advancedsearch.php?q=mediatype%3Atexts&fl%5B%5D=identifier&fl%5B%5D=oclc-id&fl%5B%5D=lccn&fl%5B%5D=issn&fl%5B%5D=isbn&fl%5B%5D=volume&sort%5B%5D=&rows=9999999&page=1&output=csv&save=yes'
 
 ia-oclc-lccn-issn-isbn-vol-pub.csv:
-	curl -o $@ 'https://archive.org/advancedsearch.php?q=mediatype%3Atexts&fl%5B%5D=identifier&fl%5B%5D=oclc-id&fl%5B%5D=lccn&fl%5B%5D=issn&fl%5B%5D=isbn&fl%5B%5D=volume&fl%5B%5D=published&sort%5B%5D=&rows=9999999&page=1&output=csv&save=yes'
-
-ia-volumes.csv:
-	curl -o $@ 'https://archive.org/advancedsearch.php?q=mediatype%3Atexts&fl%5B%5D=identifier&fl%5B%5D=volume&sort%5B%5D=&rows=9999999&page=1&output=csv&save=yes'
+	curl -o $@ 'https://archive.org/advancedsearch.php?q=mediatype%3Atexts&fl%5B%5D=identifier&fl%5B%5D=oclc-id&fl%5B%5D=lccn&fl%5B%5D=issn&fl%5B%5D=isbn&fl%5B%5D=volume&fl%5B%5D=date&sort%5B%5D=&rows=9999999&page=1&output=csv&save=yes'
 
 clean:
 	rm -fv alignment.csv alignment-unique.csv
--- book-aligner.rb
+++ book-aligner.rb
@@ -4,11 +4,13 @@ Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
 require 'csv'
+require 'time'
 
-internet_archive, internet_archive_volumes_csv, hathifile = ARGV
+internet_archive, hathifile = ARGV
 
 identifiers = {}
 ia_volumes = {}
+ia_published = {}
 
 def check_volumes(ht_vol, ia_vol)
   if ht_vol == ia_vol
@@ -33,14 +35,23 @@ def check_volumes(ht_vol, ia_vol)
   return false
 end
 
-def check_identifiers(identifiers, ia_volumes, volume_identifier, hathi_volume, hathi_identifiers, identifier_key)
+def check_published(ht_pub, ia_pub)
+  return ht_pub == ia_pub
+end
+
+def check_identifiers(identifiers, ia_volumes, ia_published, volume_identifier, hathi_volume, hathi_published, hathi_identifiers, identifier_key)
   if !hathi_identifiers.nil? && !hathi_identifiers.empty?
     hathi_identifiers.split(',').each do |hathi_identifier|
       if identifiers[identifier_key].has_key?(hathi_identifier)
         identifiers[identifier_key][hathi_identifier].each do |ia_identifier|
-          if !hathi_volume.nil? && !hathi_volume.empty? && !ia_volumes[ia_identifier].nil? && !ia_volumes[ia_identifier].empty?
-            if check_volumes(hathi_volume, ia_volumes[ia_identifier])
-              puts [volume_identifier, hathi_volume, ia_identifier, ia_volumes[ia_identifier]].join(',')
+          # if !hathi_volume.nil? && !hathi_volume.empty? && !ia_volumes[ia_identifier].nil? && !ia_volumes[ia_identifier].empty?
+          #  if check_volumes(hathi_volume, ia_volumes[ia_identifier])
+          #    puts [volume_identifier, hathi_volume, ia_identifier, ia_volumes[ia_identifier]].join(',')
+          #  end
+          # $stderr.puts [volume_identifier, hathi_published, ia_identifier, ia_published[ia_identifier]].join(' ')
+          if !hathi_published.nil? && !hathi_published.empty? && !ia_published[ia_identifier].nil? && !ia_published[ia_identifier].empty?
+            if check_published(hathi_published, ia_published[ia_identifier])
+              puts [volume_identifier, hathi_published, ia_identifier, ia_published[ia_identifier]].join(',')
             end
           else
             puts [volume_identifier, hathi_volume, ia_identifier, ia_volumes[ia_identifier]].join(',')
@@ -53,6 +64,16 @@ end
 
 $stderr.puts "Parsing Internet Archive metadata..."
 CSV.foreach(internet_archive, :headers => true) do |row|
+  ia_volumes[row['identifier']] = row['volume']
+  unless row['date'].nil? || row['date'].empty?
+    begin
+      # ia_published[row['identifier']] = Time.parse(row['date']).year.to_s
+      # Just take the first digit string as year, since IA date metdata is…problematic
+      ia_published[row['identifier']] = /^(\d+)/.match(row['date']).captures.first
+    rescue Exception => e
+      $stderr.puts(e.message + "\nSkipping #{row['date']} for #{row['identifier']}")
+    end
+  end
   %w{oclc-id lccn issn isbn}.each do |identifier_type|
     if !row[identifier_type].nil? && !row[identifier_type].empty?
       identifiers[identifier_type] ||= {}
@@ -62,15 +83,10 @@ CSV.foreach(internet_archive, :headers => true) do |row|
   end
 end
 
-$stderr.puts "Parsing Internet Archive volume metadata..."
-CSV.foreach(internet_archive_volumes_csv, :headers => true) do |row|
-  ia_volumes[row['identifier']] = row['volume']
-end
-
 $stderr.puts "Parsing HathiTrust metadata..."
 CSV.foreach(hathifile, :headers => false, :col_sep => "\t", :quote_char => "\u{FFFF}") do |row|
-  check_identifiers(identifiers, ia_volumes, row[0], row[4], row[7], 'oclc-id')
-  check_identifiers(identifiers, ia_volumes, row[0], row[4], row[8], 'isbn')
-  check_identifiers(identifiers, ia_volumes, row[0], row[4], row[9], 'issn')
-  check_identifiers(identifiers, ia_volumes, row[0], row[4], row[10], 'lccn')
+  check_identifiers(identifiers, ia_volumes, ia_published, row[0], row[4], row[16], row[7], 'oclc-id')
+  check_identifiers(identifiers, ia_volumes, ia_published, row[0], row[4], row[16], row[8], 'isbn')
+  check_identifiers(identifiers, ia_volumes, ia_published, row[0], row[4], row[16], row[9], 'issn')
+  check_identifiers(identifiers, ia_volumes, ia_published, row[0], row[4], row[16], row[10], 'lccn')
 end
